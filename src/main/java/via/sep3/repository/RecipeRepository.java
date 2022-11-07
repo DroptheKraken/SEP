9bb28034948f0bbebff83cb5cce9405e49085955
Alex Toth <tothilian@gmail.com>
2022-11-07 17:45:20 +0100

    RESTApi done, grpc-next

--- src/main/java/via/sep3/repository/RecipeRepository.java
+++ src/main/java/via/sep3/repository/RecipeRepository.java
@@ -1,39 +1,52 @@
 package via.sep3.repository;
 
 import org.springframework.stereotype.Repository;
-import via.sep3.model.Recipe;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class RecipeRepository
 {
-    private static final List<Recipe> recipes = List.of(
-            new Recipe("Chicken", new String[]{"Chicken", "salt", "pepper", "oil"}, "Fry the chicken in a pan with oil"),
-            new Recipe("Pasta", new String[]{"Pasta", "salt", "water",}, "Boil the pasta in a pot with water"),
-            new Recipe("Rice", new String[]{"Rice", "salt", "water"}, "Boil the rice in a pot with water"),
-            new Recipe("Bread", new String[]{"Bread", "salt", "pepper", "oil"}, "Bake the bread in the oven"));
+    private static final List<Recipe> recipes = new ArrayList<>();
+
+    static
+    {
+        initDataSource();
+    }
+
+    private static void initDataSource()
+    {
+        recipes.add(new Recipe("Chicken", new String[]{"Chicken", "salt", "pepper", "oil"}, "Fry the chicken in a pan with oil"));
+        recipes.add(new Recipe("Pasta", new String[]{"Pasta", "salt", "water",}, "Boil the pasta in a pot with water"));
+        recipes.add(new Recipe("Rice", new String[]{"Rice", "salt", "water"}, "Boil the rice in a pot with water"));
+        recipes.add(new Recipe("Bread", new String[]{"Bread", "salt", "pepper", "oil"}, "Bake the bread in the oven"));
+
+
+    }
 
     public void saveRecipe(Recipe recipe)
     {
-        recipes.add(recipe
-        );
+        recipes.add(recipe);
     }
 
 
-    public Recipe getByIngredient(String ingredient)
+    public ArrayList<Recipe> getByIngredient(String ingredient)
     {
+        ArrayList<Recipe> recipes1 = new ArrayList<>();
+
         for (Recipe recipe : recipes)
         {
             for (String ingridient : recipe.getIngridient())
             {
                 if (ingridient.equals(ingredient))
                 {
-                    return recipe;
+                    recipes1.add(recipe);
                 }
             }
         }
-        return null;
+        return recipes1;
     }
 
 
