ecdb048a1287f4220b5acb36bb14ad23b76907e4
ANdi g <95663488+DroptheKraken@users.noreply.github.com>
2022-11-08 23:47:44 +0100

    Big Commit - Blazor

---
+++ Tier1/SEP3_Tier1/BlazorAppTier1/CLient/Connection.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using BlazorAppTier1.Pages;
+
+namespace BlazorAppTier1.CLient;
+
+public class Connection
+{
+    public static async Task<String> FetchRecipeAsync()
+    {
+        HttpClientHandler clientHandler = new HttpClientHandler();
+    
+        clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+        using HttpClient client = new HttpClient(clientHandler);
+        HttpResponseMessage responseMessage = await client.GetAsync("https://localhost:8080/recipes/v1/recipes");
+        
+        if(!responseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception("Error");
+        }
+        
+        string result = await responseMessage.Content.ReadAsStringAsync();
+
+        String recipes = JsonSerializer.Deserialize<String>(result, new JsonSerializerOptions
+        {
+
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        });
+
+        Console.WriteLine(recipes);
+        return recipes;
+    }
+}
---
+++ Tier1/SEP3_Tier1/BlazorAppTier1/CLient/Recipe.cs
@@ -0,0 +1,48 @@
+﻿namespace BlazorAppTier1.CLient;
+
+public class Recipe
+{
+    private readonly string name;
+    private readonly string[] ingridient;
+    private readonly string description;
+
+    public Recipe(string name, string[] ingridient, string description)
+    {
+        this.name = name;
+        this.ingridient = ingridient;
+        this.description = description;
+    }
+
+    public virtual string[] Ingridient
+    {
+        get
+        {
+            return ingridient;
+        }
+    }
+
+    public virtual string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public virtual string Description
+    {
+        get
+        {
+            return description;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Recipe{" +
+               "name='" + name + '\'' +
+               ", ingridient=" + ingridient +
+               ", description='" + description + '\'' +
+               '}';
+    }
+}
