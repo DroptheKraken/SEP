0a0a67badfae227dd0f9adaa9ee3f96f149a38d9
ANdi g <95663488+DroptheKraken@users.noreply.github.com>
2022-12-12 23:51:43 +0100

    Recipes testing

---
+++ Tier1/SEP3_Tier1/Domain/DTOs/RecipeCreationDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.DTOs;
+
+public class RecipeCreationDto
+{
+    public int OwnerId { get; }
+    public string Title { get; }
+
+    public RecipeCreationDto(int ownerId, string title)
+    {
+        OwnerId = ownerId;
+        Title = title;
+    }
+}
---
+++ Tier1/SEP3_Tier1/HttpClients/ClientImpls/RecipeHttpClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http.Json;
+using Domain.DTOs;
+using HttpClients.ClientInterfaces;
+
+namespace HttpClients.ClientImpls;
+
+public class RecipeHttpClient : IRecipeInterface
+{
+    private readonly HttpClient client;
+
+    public RecipeHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+
+
+    public async Task CreateAsync(RecipeCreationDto dto)
+    {
+        HttpResponseMessage response = await client.PostAsJsonAsync("/todos", dto);
+        if (!response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+
+    }
+}
